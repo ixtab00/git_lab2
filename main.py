031fd13d1b0148e85bb0b3b48d29677a261a8c72
Alex Smirnov <alxetrapix@gmail.com>
2022-09-28 07:30:52 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from divide_and_mul import mul
+
+def main() -> None:
+    a = float(input())
+    b = float(input())
+    print(sub(a, b))
+    print(summ(a, b))
+    print(mul(a, b))
+
+
+def summ(a: float, b: float) -> float:
+    return a + b
+
+
+def sub(a: float, b: float) -> float:
+    return a - b
+
+main()
